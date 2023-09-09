17650c84c523e8ac64bf596407966b79adf094c2
danielgil <danistcruz@gmail.com>
2023-09-09 15:11:44 -0500

    Proyecto base

---
+++ src/main/java/com/co/nexos/mercancia/SistemaDeControlDeMercanciaApplication.java
@@ -0,0 +1,13 @@
+package com.co.nexos.mercancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControlDeMercanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControlDeMercanciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/nexos/mercancia/SistemaDeControlDeMercanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.nexos.mercancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControlDeMercanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
